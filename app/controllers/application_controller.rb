cfd6c44be30d46962db65f109a0bb533aa794489
Justin Kenney <justinkenney13@gmail.com>
2018-11-14 17:42:07 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,9 +24,9 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
-  # post '/recipes/:id/delete' do
-  #   @recipe = Recipe.find(params[:id]).delete
-  #   redirect to("/recipes")
-  # end
+  post '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id]).delete
+    redirect to("/recipes")
+  end
 
 end
